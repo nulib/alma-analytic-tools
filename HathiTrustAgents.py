14720f82622f95b7aa0b8c0f1465da702615be35
Kate Deibel <deibel@uw.edu>
2014-11-24 11:51:49 -0800

    Commit of new files for doing the HathiTrust downloads.

---
+++ HathiTrustAgents.py
@@ -0,0 +1,157 @@
+# HathiTrustAgents.py
+# -*- coding: utf-8 -*-
+
+from AnalyticAgent import AnalyticAgent
+
+import codecs
+import re
+
+class BaseHathiAgent(AnalyticAgent):
+    PRINT_TITLES = False    
+    def __init__(self):
+        AnalyticAgent.__init__(self)
+        self.noOCLC = []
+        self.StillLoadingSleep = 45
+        self.ErrorSleep = 45
+        self.SleepNoise = 10
+
+    @staticmethod
+    def data_filename(stem,id=None):
+        """
+        HOOK: Static method that returns a filename for the type of data
+        that this agent will produce.
+
+        As this Agent currently outputs xml, it generates filenames of
+        the form: stem[-id].xml
+
+        Parameters:
+          stem  The filestem to be used
+          id    An identification number/symbol
+
+        Returns:
+          If id is None: stem.xml
+          Else:          stem-id.xml
+
+        """
+        if id is None:
+            return stem + u'.tsv'
+        else:
+            return stem + u'-' + unicode(id) + u'.tsv'
+
+    def pre_process(self):
+        self.noOCLC = []
+
+    def row_process(self, data):
+         oclc = self.extractOCLC(data.get("Raw_OCLC", ""))
+         if oclc is None:
+             tup = ( data.get(self.Request.uniqueID), data.get("Title") )
+             self.noOCLC.append(tup)
+             return False
+         else:
+             data["OCLC"] = oclc
+
+         if data.get("Raw_ISSN", None) is not None:
+             data["ISSN"] = self.formatISSN(data.get("Raw_ISSN"))
+
+         self._writer.write(self.hathiPrint(data, BaseHathiAgent.PRINT_TITLES) + u'\n')
+         return True
+
+    def post_process(self):
+        # write the unique IDs of items missing OCLC numbers
+        filename = "no-oclc-" + self._writer.name
+        file = codecs.open(filename, 'w', encoding='utf-8')
+        file.write( unicode(self.Request.uniqueID) + u"\t"
+                    + u"Title" + u"\n")
+        for id, title in self.noOCLC:
+            file.write(unicode(id) + u"\t" + unicode(title) + u"\n")
+        file.close()
+
+        
+        # print out noOCLC list
+
+    def hathiPrint(self, data, printTitle):
+        return "NOT IMPLEMENTED!!!\n"
+
+    def extractOCLC(self, raw_oclc):
+        """
+        Attempt to get the first "best" OCLC. Best is defined as the
+        first number that has the (OCoLC) leader and just the numbers.
+        """
+        oclc = None
+        if raw_oclc is None:
+            return None
+
+        for tok in raw_oclc.split(";"):
+            for t in tok.split(" "):
+                m = re.match("\(OCoLC\)[0-9]+", t)
+                if m is not None:
+                    # found the answer
+                    parts = m.group(0).partition(")")
+                    oclc = parts[0] + parts[1] + parts[2].zfill(8)
+                    return oclc
+        # if we reach here, we've screwed up or no OCLC
+        return oclc
+
+    def formatISSN(self, raw_issn):
+        # replace ; with commas
+        issn = raw_issn.replace(";", ",")
+        # remove internal spaces
+        issn = issn.replace(" ", "")
+        return issn
+
+        
+class HathiSerialAgent(BaseHathiAgent):
+    def __init__(self):
+        BaseHathiAgent.__init__(self)
+
+    def hathiPrint(self, data, printTitle):
+        oclc = data.get("OCLC")
+        issn = data.get("ISSN", "")
+        localid = data.get("MMS_ID")
+        title = data.get("Title")
+        govid = "" # we don't track this so it's always empty
+
+        items = [ oclc, localid, issn, govid ]
+        if printTitle:
+            items.append(title)
+
+        return  u'\t'.join(items)
+
+class HathiMPMAgent(BaseHathiAgent):
+    def __init__(self):
+        BaseHathiAgent.__init__(self)
+
+    def hathiPrint(self, data, printTitle):
+        title = data.get("Title", "")
+        oclc = data.get("OCLC", "")
+        localid = data.get("Holding_ID") + u"/" + data.get("MMS_ID")
+        status = "" # we drop missing/lost eventually, so skip this
+        condition = "" # we don't track this one so it's always empty
+        chronology = data.get("Summary_Holding", "")
+        govid = "" # we don't track this so it's always empty
+
+        items = [ oclc, localid, status, condition, chronology, govid ]
+        if printTitle:
+            items.append(title)
+
+        return  u'\t'.join(items)
+
+class HathiSPMAgent(BaseHathiAgent):
+    def __init__(self):
+        BaseHathiAgent.__init__(self)
+
+    def hathiPrint(self, data, printTitle):
+        title = data.get("Title", "")
+        oclc = data.get("OCLC", "")
+        localid = data.get("Item_ID") + \
+                  u"/" + data.get("Holding_ID") + \
+                  u"/" + data.get("MMS_ID")
+        status = "" # we drop missing/lost eventually, so skip this
+        condition = "" # we don't track this one so it's always empty
+        govid = "" # we don't track this so it's always empty
+
+        items = [ oclc, localid, status, condition, govid ]
+        if printTitle:
+            items.append(title)
+
+        return  u'\t'.join(items)
---
+++ hathi_download.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+"""
+Retrieve all data (even more than 65001 rows) from an Alma analytic
+
+This file is an example of how to perform query all with optional
+multithreading by using a QueryFactory along with RequestObject and
+AnalyticAgent.
+
+This script requires the user to provide an input file for a RequestObject.
+The RequestObject will be loaded as a complex request. 
+"""
+
+##########################################################################
+# Copyright (c) 2014 Katherine Deibel
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+##########################################################################
+
+from __future__ import print_function
+
+import codecs, getopt, os, sys
+
+from AnalyticAgent import AnalyticAgent
+from HathiTrustAgents import BaseHathiAgent, HathiSerialAgent, HathiMPMAgent, HathiSPMAgent
+from RequestObject import RequestObject
+from QueryFactory import QueryFactory
+
+
+# List the options/flags for query_analytic to make it easier to
+# generate the help message.
+options = {}
+options[u"-h, --help"] = u"Display this help and exit."
+options[u"-d, --debug"] = u"Print titles in the output."
+options[u"-n, --threads=N"] = u"Run the query using N parallel threads. Default: 1."
+options[u"-q, --quiet"] = u"Quiet mode. Logging only written to file and not the screen."
+options[u"-r, --resume"] = u"Re-run the request but skip any previously completed jobs."
+
+required = {}
+required[u"--serial"] = u"Download serial holdings (hathi_inputs/hathi_serials.txt)."
+required[u"--spm"] = u"Download single-part monograph holdings (hathi_inputs/hathi_spms.txt)."
+required[u"--mpm"] = u"Download multi-part monograph holdings (hathi_inputs/hathi_mpms.txt)."
+
+def print_help():
+    """Print the help message."""
+    print(u"Usage: hathi_download.py [options] [holding type]")
+    
+    print(u"Options:")
+
+    _opts = options.keys()
+    _opts.sort()
+    # get the length of the longest key for formatting purposes
+    n = len( max(_opts, key=len) )
+
+    for o in _opts:
+        print( u" " + o.ljust(n+2,u' ') + options.get(o) )
+
+    print(u"And one of the following:")
+    _reqs = required.keys()
+    _reqs.sort()
+    for r in _reqs:
+        print( u" " + r.ljust(n+2,u' ') + required.get(r) )
+
+
+def main(argv):
+    """Main method that processes the command line, loads the
+    request object, initializes the factory, and then starts
+    it up.
+    """
+    # process the command line    
+    shortopts = "hdqrn:"
+    longopts = ["help","debug", "quiet", "threads=", "resume", "serial", "mpm", "spm"]
+    try:
+        opts, args = getopt.gnu_getopt(argv, shortopts, longopts)
+    except getopt.GetoptError as err:
+        print(unicode(err), file=sys.stderr)
+        print_help()
+        sys.exit(2)
+
+    # check that a filename was not provided
+    if len(args) != 1: # query_analytic.py, filename
+        print_help()
+        sys.exit(2)
+
+    # set features according to options
+    allowZeros = False
+    numThreads = 1
+    screenLogging = True
+    resumeWork = False
+    printTitles = False
+    fileStem = "data"
+    holdingType = None
+    agtClass = AnalyticAgent
+   
+    # process the options now              
+    for opt, arg in opts:
+        if opt == "-h" or opt == "--help":
+            print_help()
+            sys.exit(0)
+        elif opt == "-n" or opt == "--threads":
+            numThreads = int(arg)
+            if numThreads < 1:
+                print(u"The number of threads must be at least 1!",
+                      file=sys.stderr)
+                sys.exit(2)
+        elif opt == "-q" or opt == "--quiet":
+            screenLogging = False
+        elif opt == "-r" or opt == "--resume":
+            resumeWork = True
+        elif opt == "-d" or opt == "--debug":
+            printTitles = True            
+        elif opt == "-z" or opt == "--zero":
+            allowZeros = True
+        elif opt == "--serial":
+            holdingType = "serial"
+            agtClass = HathiSerialAgent
+        elif opt == "--spm":
+            holdingType = "spm"
+            agtClass = HathiSPMAgent            
+        elif opt == "--mpm":
+            holdingType = "mpm"
+            agtClass = HathiMPMAgent
+
+    # check that only one holding type is given
+    n = 0
+    for opt, arg in opts:
+        if opt in ['--serial', '--spm', '--mpm' ]:
+            n = n + 1
+    if n == 0:
+        print(u"You must include a holding type: --serial, --spm, or --mpm",
+              file=sys.stderr)
+        sys.exit(2)
+    elif n > 1:
+        print(u"You can only request one holding type at a time!",
+              file=sys.stderr)
+
+    # set various parameters based on the holding type
+    requestFile = "hathi_inputs/hathi_" + holdingType + "s.txt"
+    fileStem = holdingType
+    
+    # attempt to load the request object
+    try:
+        request = RequestObject.fromFilename(requestFile,simpleRequest=False)
+    except Exception as e:
+        lines = unicode(e).splitlines()
+        if len(lines) == 1:
+            print(u"Formatting error detected.", file=sys.stderr)
+            print(u"--> " + lines[0], file=sys.stderr)
+        else:
+            print( lines[0], file=sys.stderr)
+            for line in lines[1:]:
+                print(u"--> " + line, file=sys.stderr)
+        sys.exit(1)
+
+    print(u"Loaded analytic request: " + requestFile)
+    numJobs = len(request.JobBounds) - 1
+
+    print(u"Jobs to complete: " + unicode(numJobs))
+        
+    # check the numbers with the processors
+    if numJobs < numThreads:
+        numThreads = numJobs
+        print(u"Parallel threads: " + unicode(numThreads) \
+              + u" (reduced to number of jobs)")
+    else:
+        print(u"Parallel threads: " + unicode(numThreads))
+
+    print(u"Output files: ")
+    print(u"   " + unicode(numJobs) + " data file(s): " \
+          +agtClass.data_filename(fileStem,u'#'))
+
+    print(u"   " + u"1 file mapping job numbers to bounds: " \
+          + QueryFactory.jobmap_filename(fileStem) )
+
+    print(u"   " + u"1 log file: " 
+          + QueryFactory.log_filename(fileStem) )
+
+    print(u"   " + unicode(numJobs) + " no OCLC file(s): " \
+          + agtClass.data_filename("no-oclc-" + fileStem,u'#'))
+
+
+    # before starting the factory, set the BaseHathiAgent for debugging
+    BaseHathiAgent.PRINT_TITLES = printTitles
+
+    factory = QueryFactory.setup(request=request, num_workers=numThreads, 
+                           file_stem=fileStem, to_standard_out=screenLogging, 
+                           allow_zeros=allowZeros, resume_work=resumeWork, 
+                           agent_class=agtClass)
+    print()
+    print(u"Grabbing Analytic data (press Ctrl+C to quit)...")
+    print()
+
+    if factory.engage():
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main(sys.argv)
